03281cab9ff656e8daa2e04e2ff478fd0881d56e
panigrpr <panigrpr@FLDOLEETQM.asiapacific.hpqcorp.net>
2020-08-06 17:25:51 +0530

    initial commit

---
+++ src/main/java/com/prakash/cloud/ZuulServiceApplication.java
@@ -0,0 +1,22 @@
+package com.prakash.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+
+//@EnableEurekaClient
+//We can have the same @EnableDiscoveryClient here
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+}
